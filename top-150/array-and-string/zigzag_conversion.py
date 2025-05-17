f2a123abe2419b61daa2db084c24ec1bee411419
asatpathy314 <asatpathy314@gmail.com>
2025-05-17 16:08:57 -0400

    zigzag conversion

---
+++ top-150/array-and-string/zigzag_conversion.py
@@ -0,0 +1,34 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+
+        final_string = ""
+        row = 0
+        increment = (numRows - 1) + (numRows - 2) + 1
+        i = 0
+        odd_inc = True
+
+        while len(final_string) != len(s):
+            if i >= len(s):
+                row += 1
+                odd_inc = True
+                i = row
+            final_string += s[i]
+            if row == 0 or row == numRows - 1:
+                i = i + increment
+            elif odd_inc:
+                print(s[i], i, increment - row * 2)
+                i = i + increment - row * 2
+                odd_inc = False
+            else:
+                print(s[i], i, row * 2)
+                i = i + row * 2
+                odd_inc = True
+
+        return final_string
+
+
+"""
+Confusing problem. Figured it out. Just a traversal pattern can determine manually. 
+"""
